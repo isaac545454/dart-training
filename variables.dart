671017a98ca28d59e07a8fbfd600a75e9e10cd2e
isaac545454 <isaac.newton1221@gmail.com>
2023-03-10 06:44:47 -0300

    feat: new function

--- variables.dart
+++ variables.dart
@@ -1,4 +1,6 @@
 main() {
   int today = 2;
+  double b = 2.333;
+
   print(today);
 }
---
+++ variables_2.dart
@@ -0,0 +1,8 @@
+void main() {
+  var a = 2;
+  var b = 2.222222;
+
+  var texto = "O valor da soma é: ";
+
+  print(texto + (a + b).toString());
+}
